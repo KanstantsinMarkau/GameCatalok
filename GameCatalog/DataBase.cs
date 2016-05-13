ca7dc6551486a05e73fae07b371d4cc4fe514902
kosta <velessakharov@gmail.com>
2016-05-13 23:48:57 +0300

    Revert "Revert "IRepository""
    This reverts commit 4245f965a83d7cf55217097124eb29dc6f1764eb.

---
+++ GameCatalog/DataBase.cs
@@ -0,0 +1,76 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GameCatalog
+{
+    class DataBase
+    {
+        public void createDB()
+        {
+            //name - gamecatalog
+
+        }
+        public void createTable()
+        {
+            //name - games
+
+        }
+        public void addGame(string request)
+        {
+            string ConnString ="Database=gamecatalog;Server=localhost;Uid=root;Pwd=3128314";
+            MySqlConnection SqlConn;
+            string req = "insert into games values " + request;
+            try
+            {
+                SqlConn = new MySqlConnection(ConnString);
+                SqlConn.Open();
+                MySqlCommand myCom = new MySqlCommand();
+                myCom.CommandText = req;
+                myCom.Connection = SqlConn;
+
+                myCom.ExecuteNonQuery();
+                SqlConn.Close();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+        public void deleteGame(string request)
+        {
+            string query = "delete from games where id='" + request +"';";
+            string ConnString ="Database=gamecatalog;Server=localhost;Uid=root;Pwd=3128314";
+            MySqlConnection SqlConn;
+            try
+            {
+                SqlConn = new MySqlConnection(ConnString);
+                SqlConn.Open();
+                MySqlCommand myCom = new MySqlCommand();
+                myCom.CommandText = query;
+                myCom.Connection = SqlConn;
+                myCom.ExecuteNonQuery();
+                SqlConn.Close();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+        public List<Game> showGames()
+        {
+
+        }
+        public Game findGame()
+        {
+
+        }
+
+    }
+
+   
+}
+*/
